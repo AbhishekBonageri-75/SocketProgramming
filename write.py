87ed56786879a096aeecc5a6be0e4eef05f7ba34
Abhishek Bonageri <abonageri@hawk.iit.edu>
2022-04-24 17:55:21 -0500

    removed exit()

--- write.py
+++ write.py
@@ -11,15 +11,13 @@ def wdata(data_to_write):
         file1 = open('myfile.txt', 'r')
         print(file1.read())
         file1.close()
-        sys.exit("--")
 
     elif(inp == "2"):
         file1 = open('myfile.txt', 'r')
         print(file1.readlines()[-2])
         print(file1.readlines()[-1])
         file1.close()
-        sys.exit("--")
+
     else:
         print("Invalid input")
-        sys.exit("--")
 
