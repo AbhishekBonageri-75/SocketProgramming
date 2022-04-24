66271d54327270d25ac1a8846d42c770324f97be
Abhishek Bonageri <abonageri@hawk.iit.edu>
2022-04-24 17:08:47 -0500

    added checksum and writeToFile

---
+++ check.py
@@ -0,0 +1,20 @@
+def checksum(str_):
+
+    str_ = bytearray(str_)
+    csum = 0
+    countTo = (len(str_) // 2) * 2
+
+    for count in range(0, countTo, 2):
+        thisVal = str_[count+1] * 256 + str_[count]
+        csum = csum + thisVal
+
+    if countTo < len(str_):
+        csum = csum + str_[-1]
+
+    csum = (csum >> 16) + (csum & 0xffff)
+    csum = csum + (csum >> 16)
+    answer = ~csum
+    answer = answer & 0xffff
+    answer = answer >> 8 | (answer << 8 & 0xff00)
+
+    return answer
---
+++ write.py
@@ -0,0 +1,23 @@
+def wdata(data_to_write):
+    file1 = open('myfile.txt', 'a')
+    file1.writelines(data_to_write)
+    file1.writelines("\n")
+    file1.close()
+
+    # print the most recent ping response to the screen
+    inp = input("1.Complete file \n2.Most recent ping")
+    if(inp == "1"):
+        file1 = open('myfile.txt', 'r')
+        print(file1.read())
+        file1.close()
+
+    elif(inp == "2"):
+        file1 = open('myfile.txt', 'r')
+        print(file1.readlines()[-1])
+        file1.close()
+    else:
+        print("Invalid input")
+
+
+da = "abc"
+wdata(da)
