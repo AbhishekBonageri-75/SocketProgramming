b0223b385322361452e5c1106d7d2a18a494f0a8
anujnivas <anujnivas@gmail.com>
2022-05-06 06:34:44 -0500

    write.py commented

--- write.py
+++ write.py
@@ -6,18 +6,18 @@ def wdata(data_to_write):
     file1.close()
 
     # print the most recent ping response to the screen
-    inp = input("1.Complete file \n2.Most recent ping")
-    if(inp == "1"):
-        file1 = open('myfile.txt', 'r')
-        print(file1.read())
-        file1.close()
-
-    elif(inp == "2"):
-        file1 = open('myfile.txt', 'r')
-        print(file1.readlines()[-2])
-        print(file1.readlines()[-1])
-        file1.close()
-
-    else:
-        print("Invalid input")
+    # inp = input("1.Complete file \n2.Most recent ping")
+    # if(inp == "1"):
+    #     file1 = open('myfile.txt', 'r')
+    #     print(file1.read())
+    #     file1.close()
+    #
+    # elif(inp == "2"):
+    #     file1 = open('myfile.txt', 'r')
+    #     print(file1.readlines()[-2])
+    #     print(file1.readlines()[-1])
+    #     file1.close()
+    #
+    # else:
+    #     print("Invalid input")
 
